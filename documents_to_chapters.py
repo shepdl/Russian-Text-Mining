e394ec7a9e611059a2d1aa277693f3530560f641
Dave Shepard (CDH) <shepard.david@gmail.com>
2016-01-27 09:47:43 -0800

    Refactor scripts to work on chunks of text

--- documents_to_chapters.py
+++ documents_to_chapters.py
@@ -6,47 +6,15 @@ import codecs
 from BeautifulSoup import BeautifulSoup
 
 
-def scan_folder(directory, extension):
-    for path, dirs, files in os.walk(directory):
-        for f in files:
-            if f.endswith(extension):
-                yield os.path.join(path, f)
+out_file = sys.stdout
 
+with open(sys.stdin) as in_file:
+    in_file_contents = in_file.read() 
+    in_html = BeautifulSoup(in_file_contents)
 
-in_dir = sys.argv[1]
-out_dir = sys.argv[2]
-
-try:
-    os.makedirs(out_dir)
-except OSError as ex:
-    pass
-
-for in_file_path in scan_folder(in_dir, '.htm'):
-    containing_dir_path = in_file_path.replace('../', '')
-    containing_directory = os.path.join(out_dir, os.path.splitext(containing_dir_path)[0])
-
-    with open(in_file_path) as in_file:
-        # Fix character encoding
-        in_file_contents = in_file.read() 
-        in_html = BeautifulSoup(in_file_contents)
-
-        chapter_counter = 0
-        
-        for chapter in in_html.findAll('div', 'chapter'):
-            # Don't create directory if there are no compatible files in it
-            try:
-                os.makedirs(containing_directory)
-            except OSError as ex:  
-                # Do nothing if the directory already exists
-                pass
-            with codecs.open('{}/{}.txt'.format(containing_directory, chapter_counter), 'w', encoding='utf-8') as out_file:
-                for line_node in chapter.findAll('p'):
-                    out_file.write(line_node.text)
-                    out_file.write('\n')
-            chapter_counter += 1
-
-        if chapter_counter > 0:
-            print containing_directory
-            print "Found {} chapters".format(chapter_counter)
-
+    for chapter in in_html.findAll('div', 'chapter'):
+        for line_node in chapter.findAll('p'):
+            out_file.write(line_node.text)
+            out_file.write('\n')
+            out_file.write('|')
 
--- remove_proper_names.py
+++ remove_proper_names.py
@@ -4,20 +4,5 @@ import glob
 
 from proper_name_filter import remove_proper_names_from_file
 
-for in_filename in glob.glob('../dostoevsky/*.txt'):
-    # in_filename = sys.argv[1]
-    # out_filename = sys.argv[2]
+sys.stdout.write(remove_proper_names_from_file(sys.stdin))
 
-    chapter_number = in_filename.split('/')[-1].split('.')[0]
-    out_filename = 'proper-name-test/{}.txt'.format(chapter_number)
-
-    with codecs.open(in_filename, encoding='utf-8') as in_file, codecs.open(
-            out_filename, 'w', encoding='utf-8'
-            ) as out_file:
-
-        for line in remove_proper_names_from_file(in_file):
-            out_file.write(line)
-
-        # for line in in_file:
-        #     out_file.write(remove_proper_names(line))
-            
